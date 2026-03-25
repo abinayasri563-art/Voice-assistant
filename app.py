bcd4f4e43de19c6417a2ceda1670bd58ddec83aa
Your Name <abinayasri563@gmail.com>
2026-03-25 03:04:54 -0700

    fixed app not defined error

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+from flask import Flask, jsonify, render_template
+import wikipedia
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
 @app.route("/ask/<query>")
 def ask(query):
     try:
         result = wikipedia.summary(query, sentences=2)
-    except Exception as e:
+    except Exception:
         result = "Sorry, I couldn't find information."
-
     return jsonify({"response": result})
